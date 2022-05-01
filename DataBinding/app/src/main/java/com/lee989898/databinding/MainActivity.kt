eee206a2b3c0847be05e31f802224c88607127f4
lee989898 <wnsrud1352@gmail.com>
2022-05-01 20:25:41 +0900

    [feat] 데이터 바인딩 구현

--- DataBinding/app/src/main/java/com/lee989898/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/lee989898/databinding/MainActivity.kt
@@ -2,27 +2,25 @@ package com.lee989898.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.lee989898.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val submitButton = findViewById<Button>(R.id.main_submit_button)
-        submitButton.setOnClickListener {
+        binding.mainSubmitButton.setOnClickListener {
             displayGreeting()
         }
     }
 
     private fun displayGreeting() {
-        val message = findViewById<TextView>(R.id.main_hello_tv)
-        val name = findViewById<EditText>(R.id.main_name_et)
-
-        val messageHello = "Hello! "+ name.text
-        message.text = messageHello
+        binding.apply {
+            mainHelloTv.text = "안녕하세요! " + mainNameEt.text
+        }
     }
 }
