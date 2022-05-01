1fcd29c508c6af6d0b0351f60a129b757908290b
lee989898 <wnsrud1352@gmail.com>
2022-05-02 00:38:09 +0900

    [init] 프로젝트 초기 설정

---
+++ LiveData/app/src/main/java/com/lee989898/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.lee989898.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.lee989898.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainActivityViewModelFactory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModelFactory = MainActivityViewModelFactory(125)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
+
+        binding.resultTextView.text = viewModel.getTotal().toString()
+
+        binding.insertButton.setOnClickListener {
+            viewModel.setTotal(binding.inputEditText.text.toString().toInt())
+            binding.resultTextView.text = viewModel.getTotal().toString()
+        }
+
+    }
+}
---
+++ LiveData/app/src/main/java/com/lee989898/myapplication/MainActivityViewModel.kt
@@ -0,0 +1,21 @@
+package com.lee989898.myapplication
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel(startingTotal: Int) : ViewModel() {
+
+    private var total = 0
+
+    init {
+        total = startingTotal
+    }
+
+    fun getTotal(): Int{
+        return total
+    }
+
+    fun setTotal(input: Int){
+        total += input
+    }
+
+}
