d8c95dcd1b64e136929e6789a324b123667fb4a6
lee989898 <wnsrud1352@gmail.com>
2022-05-01 21:42:35 +0900

    [init] 프로젝트 초기 설정

---
+++ ViewModel/app/src/main/java/com/lee989898/viewmodel/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.lee989898.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.lee989898.viewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+        }
+    }
+}
