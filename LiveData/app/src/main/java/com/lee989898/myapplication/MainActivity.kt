7e44ee7dde182244922e4e2093680906ea98f106
lee989898 <wnsrud1352@gmail.com>
2022-05-02 00:45:11 +0900

    [feat] 라이브 데이터 구현

--- LiveData/app/src/main/java/com/lee989898/myapplication/MainActivity.kt
+++ LiveData/app/src/main/java/com/lee989898/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lee989898.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.lee989898.myapplication.databinding.ActivityMainBinding
 
@@ -19,11 +20,12 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
 
-        binding.resultTextView.text = viewModel.getTotal().toString()
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTextView.text = it.toString()
+        })
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text = viewModel.getTotal().toString()
         }
 
     }
--- LiveData/app/src/main/java/com/lee989898/myapplication/MainActivityViewModel.kt
+++ LiveData/app/src/main/java/com/lee989898/myapplication/MainActivityViewModel.kt
@@ -1,21 +1,21 @@
 package com.lee989898.myapplication
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
 
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData: LiveData<Int>
+        get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
-    fun getTotal(): Int{
-        return total
-    }
-
-    fun setTotal(input: Int){
-        total += input
+    fun setTotal(input: Int) {
+        total.value = (total.value)?.plus(input)
     }
 
 }
